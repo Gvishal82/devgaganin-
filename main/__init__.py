689020625f35ef1e4d6ae1a102d9cd490f0db7f5
Gvishal82 <vishalgupta33608@gmail.com>
2024-06-08 18:34:56 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -13,12 +13,13 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 logging.getLogger("telethon").setLevel(logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None)
+API_ID = "10000844" #config("API_ID", default=None, cast=int)
+API_HASH = "776f257fc1d1f8aa4aea9dd35d10a45b" #config("API_HASH", default=None)
+BOT_TOKEN = "7456514220:AAGg5MHY8x-ull2pm9RZjGMwB0rN9t976DE" #config("BOT_TOKEN", default=None)
+SESSION = "AQCYmcwAdKJQ7HjB1Eq431Eq1zQGpVpEnNIR6cspElo6ozxpRUA6K156UWAGmKFmWWDwbehI02zfEBNBIoxNX_zUbWCicQt9ZIHT1tXqqQKKVitPjqp0GuJ8wVtCkYIQaoJjLd8xySSAS9_gZo1tV4BRn6lpeHvh0BokxM-_l2RnUvueiLea5rdEiOXfh2xcbd27TphAUcJOa7sr77xbnDlklreHtjbBn9OaVHu1BdoatRBuMjLqUB2a-_V0NSQZ6H1LwUrjfnPQiQe76N36HPSkq91bdKYuVX2VYEryZX7XzL_MHxi2MftCt8eTftTwYT7YiueJ3Jw0FMuYOKdvszo8rqRRQgAAAAGprFxBAA" #config("SESSION", default=None)
+FORCESUB = "Restricted_content_bot1" #config("FORCESUB", default=None)
+AUTH = "7141612609" #config("AUTH", default=None)
+
 SUDO_USERS = []
 
 if len(AUTH) != 0:
