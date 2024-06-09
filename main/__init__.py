6a1f58a17abac584e9ce959385443d22a8acdc76
Gvishal82 <vishalgupta33608@gmail.com>
2024-06-09 05:42:16 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -15,7 +15,7 @@ logging.getLogger("telethon").setLevel(logging.WARNING)
 # variables
 API_ID = "10000844" #config("API_ID", default=None, cast=int)
 API_HASH = "776f257fc1d1f8aa4aea9dd35d10a45b" #config("API_HASH", default=None)
-BOT_TOKEN = "7341520063:AAGMUQBtz8z5pIqDMs7p5kIOHxHKu5ECh6U" #config("BOT_TOKEN", default=None)
+BOT_TOKEN = "7277416749:AAEYE5q-IxZSlno6Jq2YXeVlwEWOr13l3BQ" #config("BOT_TOKEN", default=None)
 SESSION = "BQGtpF0Atbhw6pSY_cWvDLYDiuR5jLAcpLmp4eb7xOsGtc0c3-mVWMF__QxUwYWlskZMWRajuWAb_Sg1NHm00a3ajDMX3GsSwn-0ZzvLeWQ7x76LDqDbRFH8VyB5bPvFT9zGOzAn5JvPzsnrWE_iOZNqyCZC0fKErOmSgEPCfoDXr12OGoCZm_a-exKRuEizr9KuaOOtYw8jfhnZT3mFT4LhIPrZ8pbXgJDNGU1xBzjCRmEcfs-DWat_86JqQw5_3ovWIGXvpgRrVKMxjtlB05pA9RHHQFTTLoNXh5CXOPymZ-XmaAxBQIsu_C2YggzsN-zW6ytMLLBVSoklp-_TWpVbYxgAEwAAAAG1IkhdAA" #config("SESSION", default=None)
 FORCESUB = "Restricted_content_bot1" #config("FORCESUB", default=None)
 AUTH = "7141612609" #config("AUTH", default=None)
